f635dbde53ea5e00c7da169da4f32596ae1d74d0
saraelaela <saraelabed@outlook.de>
2024-10-23 14:54:25 +0200

    Updating and using the POST request

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,15 +1,64 @@
 import { NextResponse } from 'next/server';
-import { getUsersInsecure } from '../../../database/users';
+import { z } from 'zod';
+import {
+  createUserInsecure,
+  getUsersInsecure,
+  type User,
+} from '../../../database/users';
 
-type RootResponseBodyGet =
+type UserResponseBodyGet =
   | {
-      users: string;
+      users: User;
     }
   | {
       error: string;
     };
 
-export async function GET(): Promise<NextResponse<RootResponseBodyGet>> {
+export async function GET(): Promise<NextResponse<UserResponseBodyGet>> {
   const users = await getUsersInsecure();
   return NextResponse.json({ users: users }); // This is the endpoint
+}
+
+const userSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+  email: z.string(),
+  role: z.string(), // Add the missing fields
+  introText: z.string(),
+  profilePicture: z.string().url(),
+  createdAt: z.string(),
+});
+
+type UserResponseBodyPost =
+  | {
+      users: User;
+    }
+  | {
+      error: string;
+    };
+
+export async function POST(
+  request: Request,
+): Promise<NextResponse<UserResponseBodyPost>> {
+  const requestBody = await request.json();
+  const result = userSchema.safeParse(requestBody);
+  if (!result.success) {
+    return NextResponse.json(
+      {
+        error: 'You need to send an Object',
+      },
+      { status: 400 },
+    );
+  }
+
+  const newUser = await createUserInsecure({
+    username: result.data.username,
+    password: result.data.password,
+    email: result.data.email,
+    role: result.data.role, // Pass the role
+    introText: result.data.introText, // Pass the introText
+    profilePicture: result.data.profilePicture, // Pass the profilePicture
+    createdAt: result.data.createdAt, // Pass the createdAt
+  });
+  return NextResponse.json({ users: newUser });
 }
--- database/users.ts
+++ database/users.ts
@@ -6,10 +6,10 @@ export type User = {
   username: string;
   password: string;
   email: string | null;
-  role: string;
-  intro_text: string;
-  profile_picture: string;
-  created_at: Date;
+  role?: string;
+  intro_text?: string;
+  profile_picture?: string;
+  created_at?: Date;
 };
 
 export const getUsersInsecure = cache(async () => {
@@ -21,4 +21,33 @@ export const getUsersInsecure = cache(async () => {
   `;
 
   return users;
+});
+
+export const createUserInsecure = cache(async (newUser: Omit<User, 'id'>) => {
+  const [user] = await sql<User[]>`
+    INSERT INTO
+      users (
+        username,
+        password,
+        email,
+        role,
+        intro_text,
+        profile_picture,
+        created_at
+      )
+    VALUES
+      (
+        ${newUser.username},
+        ${newUser.password},
+        ${newUser.email},
+        ${newUser.role},
+        ${newUser.introText},
+        ${newUser.profilePicture},
+        ${newUser.createdAt}
+      )
+    RETURNING
+      users.*
+  `;
+
+  return user;
 });
