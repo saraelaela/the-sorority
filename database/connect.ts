ad023961407ff3e6bb75927cdc533d17ad874951
saraelaela <saraelabed@outlook.de>
2024-12-02 20:05:03 +0100

    Updating Server Component

--- database/connect.ts
+++ database/connect.ts
@@ -1,4 +1,4 @@
-import 'server-only';
+// import 'server-only';
 import type { Sql } from 'postgres';
 import postgres from 'postgres';
 import { postgresConfig, setEnvironmentVariables } from '../util/config';
